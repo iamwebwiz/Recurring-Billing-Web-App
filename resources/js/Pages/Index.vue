83ef4b5fb679df966c4aae81ad376286d20d1ab5
Ezekiel <iamwebwiz@gmail.com>
2020-03-16 16:30:12 +0100

    Update Landing page view

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,15 +1,49 @@
 <template>
     <div>
         <div class="container">
-            <h2>Inertia!!!</h2>
-            <p>Yay! Inertia is here!</p>
+            <div class="row justify-content-center my-5">
+                <div class="col-md-6 col-sm-10 col-12 col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <form @submit.prevent="submit">
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" v-model="form.email" autofocus id="email" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" v-model="form.password" id="password" class="form-control">
+                                </div>
+
+                                <button class="btn btn-primary" :disabled="sending">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Index"
+        name: "Index",
+        data: () => ({
+            sending: false,
+            form: {
+                email: 'test@gmail.com',
+                password: 'password',
+            }
+        }),
+        methods: {
+            submit() {
+                this.sending = true;
+                this.$inertia.post(this.route('login.attempt'), {
+                    email: this.form.email,
+                    password: this.form.password
+                }).then(() => this.sending = false);
+            }
+        }
     };
 </script>
 
