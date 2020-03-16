f3a84ce38ae6e83575a9f919c92e49baa04323f8
Ezekiel <iamwebwiz@gmail.com>
2020-03-16 14:02:01 +0100

    Initial commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="container">
+            <h2>Inertia!!!</h2>
+            <p>Yay! Inertia is here!</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index"
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import { InertiaApp } from '@inertiajs/inertia-vue';
+
+Vue.use(InertiaApp);
+
+const app = document.getElementById('app');
+
+new Vue({
+    render: h => h(InertiaApp, {
+        props: {
+            initialPage: JSON.parse(app.dataset.page),
+            resolveComponent: name => import(`./Pages/${name}`).then(module => module.default),
+        },
+    }),
+}).$mount(app);
