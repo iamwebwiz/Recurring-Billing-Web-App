df560f5f0f19f1f888607b8f95d4d60324901068
Ezekiel <iamwebwiz@gmail.com>
2020-03-17 06:25:57 +0100

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', static function () {
-    return \Inertia\Inertia::render('Index');
+    return redirect()->route('login');
 });
 
 Auth::routes();
