384ae8e134b7fc41ebbc48a1d3ffed079296d844
Ezekiel <iamwebwiz@gmail.com>
2020-03-17 13:50:00 +0100

    Add UsersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        factory(User::class, 3)->create();
+
+        $this->command->info('User accounts created successfully.');
+    }
+}
