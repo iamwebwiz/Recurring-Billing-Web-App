5a930758197311549fc9f37b7dcdc8c841eec120
Ezekiel <iamwebwiz@gmail.com>
2020-03-17 13:51:30 +0100

    Add vue sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,13 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 import { InertiaApp } from '@inertiajs/inertia-vue';
+import VueSweetalert2 from "vue-sweetalert2";
+
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.config.productionTip = false;
 Vue.use(InertiaApp);
+Vue.use(VueSweetalert2);
 
 const app = document.getElementById('app');
 
