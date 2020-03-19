4b9e77a7c18251e92aaabdfab7216960d0899e1a
Ezekiel <iamwebwiz@gmail.com>
2020-03-19 15:47:00 +0100

    Update UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        factory(User::class, 3)->create();
+        factory(User::class)->create([
+            'email' => 'afrinvest@gmail.com',
+            'name' => 'Afrinvest',
+        ]);
 
         $this->command->info('User accounts created successfully.');
     }
