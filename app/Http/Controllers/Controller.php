f51bb05eb61425d3daebba49c1cdf78742b28f73
Ezekiel <iamwebwiz@gmail.com>
2020-04-06 13:36:08 +0100

    Update api documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,23 +15,14 @@ class Controller extends BaseController
      *      title="Afrinvest Recurring Billing API",
      *      description="API documentation for recurring billing api",
      *      @OA\Contact(
-     *          email="iamwebwiz@gmail.com"
-     *      ),
-     *      @OA\License(
-     *          name="Apache 2.0",
-     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *          email="iamwebwiz@gmail.com",
+     *          name="Ezekiel Oladejo"
      *      )
      * )
      *
      * @OA\Server(
      *      url=L5_SWAGGER_CONST_HOST,
      *      description="Demo API Server"
-     * )
-
-     *
-     * @OA\Tag(
-     *     name="Recurring Billing",
-     *     description="API endpoints for recurring billing"
      * )
      */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
